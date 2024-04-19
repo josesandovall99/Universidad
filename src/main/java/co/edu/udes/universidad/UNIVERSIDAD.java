ab7f91a1848d24f4a4d512c79919a14a6fd54018
jose sandoval <luisvalr120@gmail.com>
2024-04-19 18:17:06 -0500

    inicio

---
+++ src/main/java/co/edu/udes/universidad/UNIVERSIDAD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.udes.universidad;
+
+/**
+ *
+ * @author JOSE SANDOVAL
+ */
+public class UNIVERSIDAD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
