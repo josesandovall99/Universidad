ed7de0c952ee75531792d5501e40e454cde028ca
jose sandoval <luisvalr120@gmail.com>
2024-04-20 19:56:43 -0500

    conexion a base de datos local

--- src/main/java/BD/Conexion.java
+++ src/main/java/BD/Conexion.java
@@ -4,10 +4,39 @@
  */
 package BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author JOSE SANDOVAL
  */
 public class Conexion {
-    
+
+    Connection conectar = null;
+
+    String usuario = "root";
+    String contraseña = "";
+    String bd = "universidad";
+    String ip = "localhost";
+    String puerto = "3306";
+
+    String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+
+    public Connection establecerConexion() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conectar = DriverManager.getConnection(cadena, usuario, contraseña);
+            JOptionPane.showMessageDialog(null, "CONEXION ESTABLECIDA CORRECTAMENTE");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "error  al conectarse a la bd" + e.toString());
+        }
+
+        return conectar;
+
+    }
 }
